386cee412077c5a7218695939e22745f9b38942c
subhamkr086 <154012751+subhamkr086@users.noreply.github.com>
2023-12-22 01:46:37 +0530

    Add files via upload

---
+++ ProfilePage.js
@@ -0,0 +1,708 @@
+let coordinators = [
+    {
+        name: "Aditi Raj",
+        alt: "Aditi",
+        department: "ECE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Aman Kumar Tiwary",
+        alt: "Aman",
+        department: "ECE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Amit Chandra Dey",
+        alt: "Amit",
+        department: "ECE",
+        socialMedia: {
+            facebook: "johnDoeFB",
+            insta: "johnDoeInsta",
+            linkedin: "johnDoeLinkedIn"
+        }
+    },
+    {
+        name: "Priyanshu Kumar",
+        alt: "Priyanshu_Kumar",
+        department: "ECE",
+        socialMedia: {
+            facebook: "johnDoeFB",
+            insta: "johnDoeInsta",
+            linkedin: "johnDoeLinkedIn"
+        }
+    },
+    {
+        name: "Shashank Shekhar",
+        alt: "Shashank_ECE",
+        department: "ECE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Rana Hritik",
+        alt: "Rana",
+        department: "ECE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+
+    {
+        name: "Amit Mandal",
+        alt: "Amit_Mandal",
+        department: "IT",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Sumit Kumar Singh",
+        alt: "Sumit_IT",
+        department: "IT",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Abhishek Srivastava",
+        alt: "Abhishek_Srivastava",
+        department: "IT",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Saorabh Prakash",
+        alt: "Saorabh_Prakash",
+        department: "IT",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Abhijeet Kumar Mahato",
+        alt: "Abhijeet_kumar_Mahato",
+        department: "IT",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Amisha Kumari",
+        alt: "Amisha_Kumari",
+        department: "IT",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+   
+
+    {
+        name: "Amrit Singh",
+        alt: "AMRIT_SINGH",
+        department: "CE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Rajeev Kumar Singh",
+        alt: "Rajeev_Kumar_Singh",
+        department: "CE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Divyanshu Raj",
+        alt: "Divyanshu",
+        department: "CE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Ranjeet Mandal",
+        alt: "Ranjeet_Mondal",
+        department: "CE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Supriya Verma",
+        alt: "SUPRIYA_VERMA_CE",
+        department: "CE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Issan Basu",
+        alt: "Issan_Basu",
+        department: "CE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+
+    {
+        name: "Rohit Sharma",
+        alt: "ROHIT_SHARMA",
+        department: "CSE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Shashank Sahay",
+        alt: "Shashanksce",
+        department: "CSE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Shoumik Manna",
+        alt: "Shoumik",
+        department: "CSE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Shreya Sinha",
+        alt: "Shreya",
+        department: "CSE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Sourav Jana",
+        alt: "Sourav_CSE",
+        department: "CSE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Kaushal Kishor Tiwary",
+        alt: "Kaushal",
+        department: "CSE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    
+    {
+        name: "Niwesh Kumar",
+        alt: "Niwesh_Kumar",
+        department: "AIML (CSE)",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Saumya Singh",
+        alt: "Saumya_Singh",
+        department: "AIML (CSE)",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Mushkan Sharma",
+        alt: "MuskanCSE",
+        department: "AIML (CSE)",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Nivedita Bharti",
+        alt: "Nivedita",
+        department: "AIML (CSE)",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Akash Mahato",
+        alt: "Akash_Mahato",
+        department: "AIML (CSE)",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Debtirtha Roy",
+        alt: "Debtirtha",
+        department: "AIML (CSE)",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+
+    {
+        name: "Harshek Madhukar",
+        alt: "Harshek_Madhukar",
+        department: "CSD (CSE)",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Abhinav Kumar",
+        alt: "AbhinavCSE",
+        department: "CSD (CSE)",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Anindita Laha",
+        alt: "Anindita",
+        department: "CSD (CSE)",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Shagufa Ali Haidaer",
+        alt: "Shagufa",
+        department: "CSD (CSE)",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Aditi Kumari",
+        alt: "AditiKumari",
+        department: "CSD (CSE)",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+
+    {
+        name: "Angel",
+        alt: "Angel",
+        department: "DS (CSE)",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Sohini Naha",
+        alt: "Sohini",
+        department: "DS (CSE)",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Kishlay Babul",
+        alt: "Kishlay",
+        department: "DS (CSE)",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Satyam Satsang",
+        alt: "SATYAM_SATSANG",
+        department: "ECE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Krishna Thakur",
+        alt: "Krishna",
+        department: "DS (CSE)",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Aditya Raj Harsh",
+        alt: "Aditya_Raj_Harsh",
+        department: "DS (IT)",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+
+    {
+        name: "Anshu Kumar",
+        alt: "ANSHU_KUMAR",
+        department: "EE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Satyam Kumar",
+        alt: "Satyam_kumar",
+        department: "EE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Vikash Kumar",
+        alt: "Vikash",
+        department: "EE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "SK Moin",
+        alt: "SK_Moin",
+        department: "EE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Muskan Tiwari",
+        alt: "Muskan_Tiwari",
+        department: "EE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Ashish Kumar",
+        alt: "Ashish",
+        department: "EE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+
+    {
+        name: "Rajnish Kumar Ranjan",
+        alt: "RAJNISH_KUMAR_RANJAN",
+        department: "ME",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Sameer Singh",
+        alt: "Sameer_Singh",
+        department: "ME",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Piyush Joshi",
+        alt: "Piyush",
+        department: "ME",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Prince Kumar",
+        alt: "PRINCE_KUMAR",
+        department: "ME",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Ayan Banerjee",
+        alt: "AYAN_BANERJEE",
+        department: "ME",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Amit Kumar",
+        alt: "Amit_Kumar",
+        department: "ME",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+
+    {
+        name: "Pranav Raj",
+        alt: "Pranav_Raj",
+        department: "MBA",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Ashish Kumar Singh",
+        alt: "Ashish",
+        department: "IT",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Robin Kumar Chatterjee",
+        alt: "Robin_Kumar_Chatterjee",
+        department: "MBA",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Rohit Tiwary",
+        alt: "Rohi-tiwari",
+        department: "MBA",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Sohan Chakraborty",
+        alt: "Sohan_Chakraborty",
+        department: "MBA",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Atif Raza",
+        alt: "Atif",
+        department: "ECE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+
+    {
+        name: "Anurag Kumar",
+        alt: "Anurag",
+        department: "MCA",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Pradip Kumar Das",
+        alt: "Pradip",
+        department: "MCA",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Sudipto Singha",
+        alt: "SudiptoSingha",
+        department: "ECE",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Chandra Sekhar Bala",
+        alt: "ChandraSekhar",
+        department: "MCA",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Avijit Mandal",
+        alt: "AvijitMondal",
+        department: "MCA",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+    {
+        name: "Abhineet Dutta",
+        alt: "AbhineetDutta",
+        department: "MCA (ECE)",
+        socialMedia: {
+            facebook: "aliceSmithFB",
+            insta: "aliceSmithInsta",
+            linkedin: "aliceSmithLinkedIn"
+        }
+    },
+
+];
+
+// console.log(coordinators.length)
+document.addEventListener('DOMContentLoaded', function() {
+    let container = document.querySelector('#coordinator_grid');
+    console.log(container)
+
+    coordinators.forEach(coordinator =>{
+        // console.log(coordinator)
+        let coordinatorDiv = document.createElement('div');
+        coordinatorDiv.classList.add('box', 'glowing');
+
+        coordinatorDiv.innerHTML = `
+            <div class="card">
+                <div class="card-image">
+                    <img src="Coordinator/${coordinator.alt}.jpg" alt="${coordinator.alt}" style="width: 100%;">
+                </div>
+                <ul class="social-icons">
+                    <li>
+                        <a href="${coordinator.socialMedia.facebook}">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="${coordinator.socialMedia.instagram}">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="${coordinator.socialMedia.linkedin}">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                    </li>
+                </ul>
+                <div class="details">
+                    <h2>${coordinator.name}<br><span class="job-title">${coordinator.department}</span></h2>
+                </div>
+            </div>
+        `;
+
+        // Append the created elements to the container
+        container.appendChild(coordinatorDiv);
+    })
+    
+});
